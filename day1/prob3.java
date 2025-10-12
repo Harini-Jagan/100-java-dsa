67295ea7146eee56069e3a34b74483b6eced8904
Harini-Jagan <harini2410@gmail.com>
2025-10-12 12:43:30 +0530

    day1

---
+++ day1/prob3.java
@@ -0,0 +1,12 @@
+
+
+public class prob3 {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<3){
+            System.out.println("while loop");
+            i++;
+        }
+    }
+    
+}
---
+++ day1/prob4.java
@@ -0,0 +1,13 @@
+
+public class prob4 {
+    public static void main(String[] args) {
+        int i=2;
+        do{
+            System.out.println("do while loop");
+            i++;
+        }
+        while(i<=4);
+    }
+    
+}
+
