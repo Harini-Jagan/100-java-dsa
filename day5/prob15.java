630cf923cf9d19945a5dc8d06249529b92a1db34
Harini-Jagan <harini2410@gmail.com>
2025-10-18 20:46:51 +0530

    day5

---
+++ day5/prob15.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class prob15 {
+    public static void main(String[] args) {
+        int score=64;
+        if(score>35){
+            System.out.println("play a video game");
+        }
+        else if(score>60){
+            System.out.println("buy dress");
+        }
+        else if(score>90){
+            System.out.println("buy iphone");
+        }
+    }
+    
+}
