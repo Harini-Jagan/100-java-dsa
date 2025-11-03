a10d92968dd746e0a5c5afa557e2afe76a383951
Harini-Jagan <harini2410@gmail.com>
2025-11-03 21:03:48 +0530

    11

---
+++ day11/prob27.java
@@ -0,0 +1,20 @@
+package day11;
+public class prob27{
+    public static void main(String[] args) {
+
+       int a=18; 
+            //post increment
+            // use first,update second
+
+       if(a++ == a){
+        System.out.println("a is equal to postincrement of a");
+       }
+       else{
+        System.out.println("a is not equal to increment of a");
+       }
+
+
+       
+
+    }
+}
---
+++ day11/prob28.java
@@ -0,0 +1,16 @@
+package day11;
+
+public class prob28 {
+    public static void main(String[] args) {
+        int b=14;
+       //pre increment
+       //update first ,use later
+       if( ++b == b){
+        System.out.println("b is equal to preincrement of b");
+       }
+       else{
+        System.out.println("b is not equal to preincrement of b");
+       }
+    }
+    
+}
